9b78290ea49d34817c8247b440c32df9a13b56e7
thornhiven <thornhiven@mail.ru>
2020-03-25 16:45:43 +0700

    Update one.go

--- one.go
+++ one.go
@@ -1,51 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
 	"encoding/json"
-	"strconv"
+	"fmt"
+	"flag"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 	"strings"
+	"net"
 )
 
-type Task struct{
-	ID    	 string
-	Method 	 string   `json:"method"`
-	Url    	 string   `json:"url"`
-	Header   string   `json:"header"`
-	Body     string   `json:"body"`
+type Task struct {
+	ID     string
+	Method string      `json:"method"`
+	Url    string      `json:"url"`
+	Header http.Header `json:"header"`
+	Body   string      `json:"body"`
 }
 
-type TaskResult struct{
-	ID 			string
+type TaskResult struct {
+	ID          string
 	StatusCode  int
 	LenResponse int64
-	Header  	http.Header
+	Header      http.Header
 }
 
+var taskList map[string]Task //список запросов
+var idCounter int = 100
 
-var taskList map[string]Task
-var id int=100
-
+//удаление запроса с заданым индексом
 func deltask(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
-	id, err := q["id"]
-	if !err {
-		http.Error(w, "", 400)
+	id, ok := q["id"]
+	if !ok {
+		http.Error(w, "id required", 400)
 		return
-	}else if _, ok:=taskList[id[0]] ; ok{
-			delete(taskList,id[0])
-			fmt.Fprintf(w,"Task deleted")
-		}else{
-			fmt.Fprintf(w,"Id not exist")
-		}
+	} else if _, ok := taskList[id[0]]; ok {
+		delete(taskList, id[0])
+		fmt.Fprintf(w, "Task deleted")
+	} else {
+		fmt.Fprintf(w, "Id not exist")
+	}
 }
 
-//todo сделать постранично
-func getTaskList(w http.ResponseWriter, r *http.Request){
-	for _,v:=range taskList{
+//возвращает список имеющихся запросов
+func getTaskList(w http.ResponseWriter, r *http.Request) {
+	for _, v := range taskList {
 		res, err := json.Marshal(v)
 		if err != nil {
 			http.Error(w, err.Error(), 500)
@@ -53,93 +55,66 @@ func getTaskList(w http.ResponseWriter, r *http.Request){
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(res)
-
 	}
-
 }
 
-func createTask(w http.ResponseWriter, r *http.Request){
-
+//считывает просьбу, выполняет и заносит в список
+func createTask(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
-
 	defer r.Body.Close()
-
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
 	var task Task
-
 	err = json.Unmarshal(body, &task)
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	//todo сделать нормальный генератор
 
-
-	task.ID=strconv.Itoa(id)
-	id++
-	strings.ToUpper(task.Method)
+	task.ID= strconv.Itoa(idCounter)
+	idCounter++
 	taskList[task.ID] = task
 
-	var resp *http.Response
-
-	switch strings.ToUpper(task.Method){
-		case "GET":		resp, err = http.Get(task.Url)
-		case "POST":	resp, err = http.Post(task.Url,task.Header,strings.NewReader(task.Body))
-		case "HEAD":	resp, err = http.Get(task.Url)
-		default:fmt.Println("Unknown method")//TODO
-	}
-
+	client := &http.Client{}
+	req, err := http.NewRequest(strings.ToUpper(task.Method), task.Url, nil)
+	req.Header = task.Header
+	resp, err := client.Do(req)
 
 	if err != nil {
-		http.Error(w, err.Error(), 400)
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
 	taskResp := TaskResult{
-		ID: task.ID,
+		ID:          task.ID,
 		StatusCode:  resp.StatusCode,
 		LenResponse: resp.ContentLength,
-		Header: resp.Header,
+		Header:      resp.Header,
 	}
 
 	res, err := json.Marshal(taskResp)
-	/*if err != nil {
-		http.Error(w, err.Error(), 400)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
 		return
-	}*/
-
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
 }
 
 func main() {
 
-	taskList=make(map[string]Task)
-	var t Task
-	t= Task{ID: "1",
-			Method: "get",
-			Url: "google.com",
-			}
-	taskList[t.ID]=t
-	t= Task{ID: "2",
-			Method: "get",
-			Url: "yandex.com",
-			}
-	taskList[t.ID]=t
-	t= Task{ID: "345",
-			Method: "post",
-			Url: "asd.ytr",
-			}
-	taskList[t.ID]=t
-	port:=":8080"
+	var port = flag.String("port", "8080", "Port value")
+	flag.Parse()
+	taskList = make(map[string]Task)
 
 	http.HandleFunc("/task", createTask)
-	http.HandleFunc("/gettasklist",getTaskList)
-	http.HandleFunc("/deltask",deltask)
+	http.HandleFunc("/gettasklist", getTaskList)
+	http.HandleFunc("/deltask", deltask)
 
-	err := http.ListenAndServe(port, nil)
+	err := http.ListenAndServe(net.JoinHostPort("localhost", *port), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
