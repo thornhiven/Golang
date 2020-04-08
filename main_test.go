18033170a1df29430ec4e55b74e20d6338c3fd0b
thornhiven <thornhiven@mail.ru>
2020-04-08 17:03:27 +0700

    Add files via upload

--- main_test.go
+++ main_test.go
@@ -5,22 +5,26 @@ import (
 	//`bytes`
 	//"encoding/json"
 
+	`bytes`
+	`net/http`
+
 	//`net/http`
 	"net/http/httptest"
 	"testing"
 
 	"github.com/labstack/echo/v4"
+	`github.com/stretchr/testify/assert`
 
 	//`github.com/stretchr/testify/assert`
 
-	"go-task/store"
+	//"go-task/store"
+
+	`go-task/store`
 )
 import "github.com/stretchr/testify/require"
 
-var tasKList = store.NewStore("map")
-
-/*
 func TestCreateTask(t *testing.T) {
+	taskList = store.NewStore("map")
 	e := echo.New()
 	body := bytes.NewBufferString(`{ "ID": "100" },
 										"method":"get",
@@ -31,19 +35,20 @@ func TestCreateTask(t *testing.T) {
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 	resp:=createTask(c)
+	tl, _:=taskList.GetAllTasks()
 	if assert.NoError(t, resp) {
-		require.Equal(t, http.StatusOK, rec.Code)
+		require.Equal(t, http.StatusOK, rec.Code, tl)
 		//require.Equal(t, userJSON, rec.Body.String())
 	}
 }
-*/
 
 func TestGetAllTasks(t *testing.T) {
+
 	e := echo.New()
 	req := httptest.NewRequest("GET", "/gettasklist", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 	er := getTaskList(c)
-	tl, _ := tasKList.GetAllTasks()
+	tl, _ := taskList.GetAllTasks()
 	require.Equal(t, tl, er)
 }
